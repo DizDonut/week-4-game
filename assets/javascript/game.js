daa3c14eee3784fbe7c83a068f66cfdd95571561
Michael R. Bechtel <mikebec20@gmail.com>
2017-06-23 18:40:18 -0400

    updated html with bootstrap

---
+++ assets/javascript/game.js
@@ -0,0 +1,14 @@
+var gameObject =
+{
+  randomTotalNumb: 0,
+  crystalNumOne: 0,
+  crystalNumTwo: 0,
+  crystalNumFour: 0,
+  crystalNumThree: 0,
+  totalScore: 0,
+  totalWins: 0,
+  totalLosses: 0,
+
+  
+
+}
